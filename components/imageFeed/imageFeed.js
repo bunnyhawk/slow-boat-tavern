54301c8ca8babf1047a923e205458cddfe1ad3ea
Robert Higdon <positronicshell@gmail.com>
2016-11-11 15:46:36 -0800

    Adding missing files

---
+++ components/imageFeed/imageFeed.js
@@ -0,0 +1,42 @@
+const imageFeed = () => {
+  const apiUrl = 'https://api.instagram.com/v1/users/slowboattavern/media/recent/?access_token=f6ce4e112e434d589a84583ed7a3ce66';
+  const toReadyStateDescription = function (state) {
+      switch (state) {
+      case 0:
+          return 'UNSENT';
+      case 1:
+          return 'OPENED';
+      case 2:
+          return 'HEADERS_RECEIVED';
+      case 3:
+          return 'LOADING';
+      case 4:
+          return 'DONE';
+      default:
+          return '';
+      }
+  };
+
+  let oReq = new XMLHttpRequest();
+  let results = document.getElementById('imageFeed');
+
+  oReq.onload = function (e) {
+      var xhr = e.target;
+      console.log('Inside the onload event');
+      if (xhr.responseType === 'json') {
+          results.innerHTML = xhr.response.message;
+      } else {
+          results.innerHTML = JSON.parse(xhr.responseText).message;
+      }
+  };
+  oReq.onreadystatechange = function () {
+      console.log('Inside the onreadystatechange event with readyState: ' + toReadyStateDescription(oReq.readyState));
+  };
+  oReq.open('GET', apiUrl, true);
+  oReq.responseType = 'json';
+  oReq.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+  oReq.setRequestHeader('x-vanillaAjaxWithoutjQuery-version', '1.0');
+  oReq.send();
+};
+
+export { imageFeed };
