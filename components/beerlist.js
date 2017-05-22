628645fa3a0d51af56816e3fd60626a4dc3ac072
slowboattavern <kenjprovost76@gmail.com>
2017-05-22 14:28:12 -0700

    Updated beerlist.js

--- components/beerlist.js
+++ components/beerlist.js
@@ -1,40 +1,40 @@
 export default {
     "list": [
         {
-            "brewery": "Holy Mountain",
-            "title": "Minor Planet, Oaked Pale",
-            "size": "12 oz",
-            "price": "6"
+            "brewery": "",
+            "title": "",
+            "size": "",
+            "price": ""
         },
         {
-            "brewery": "Holy Mountain & Chuckanut",
-            "title": "Oak Fermented Weizenbock",
-            "size": "12 oz",
-            "price": "6"
+            "brewery": "",
+            "title": "",
+            "size": "",
+            "price": ""
         },
        {
-            "brewery": "Holy Mountain",
-            "title": "The OX",
-            "size": "12 oz",
-            "price": "6"
-        },
-        {
-            "brewery": "Holy Mountain",
-            "title": "Bourbon Barrel Kings Head",
-            "size": "8 oz",
-            "price": "6"
-        },
-        {
-            "brewery": "Holy Mountain",
-            "title": "Brett Bonne Nuit",
-            "size": "12 oz",
-            "price": "6"
-        },
-        {
-            "brewery": "Holy Mountain",
-            "title": "Cathedral, Kolsch",
+            "brewery": "Reissdorf",
+            "title": "Kolsch",
             "size": "16 oz",
-            "price": "6"
+            "price": "7"
+        },
+        {
+            "brewery": "De Dolle",
+            "title": "Oerbier",
+            "size": "8 oz",
+            "price": "8"
+        },
+        {
+            "brewery": "Trinity",
+            "title": "Presidents Club",
+            "size": "8 oz",
+            "price": "8"
+        },
+        {
+            "brewery": "JW Lee's",
+            "title": "2016 Port, Harvest Ale",
+            "size": "8 oz",
+            "price": "10"
         },
         {
             "brewery": "JW Lee's",
@@ -56,7 +56,7 @@ export default {
         },
         {
             "brewery": "Block 15",
-            "title": "Stickey Hands, IPA",
+            "title": "Hop Zest, Hoppy Wheat",
             "size": "16 oz",
             "price": "6"
         },
@@ -68,7 +68,7 @@ export default {
         },
         {
             "brewery": "Cloudburst",
-            "title": "Verse,Chours,Verse, IPA",
+            "title": "Standard Operating Procedure, IPA",
             "size": "16 oz",
             "price": "6"
         },
